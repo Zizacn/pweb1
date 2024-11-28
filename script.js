22e042f9fa78bd9920c700e9d9a94b63dcc30491
Zizacn <icn2@aluno.ifal.edu.br>
2024-11-28 10:17:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var n1 = parseFloat(prompt("Digite a primeira nota:"));
+var n2 = parseFloat(prompt("Digite a segunda nota:"));
+var n3 = parseFloat(prompt("Digite a terceira nota:"));
+    
+
+    var media = (n1+n2+n3) /3
+
+    alert ("a media é:" + media)
